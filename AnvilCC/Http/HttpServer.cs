154672e8640ab65081d802d06c13a2ce5d5324d7
Alec Thilenius <alec@thilenius.com>
2014-09-24 23:08:46 -0600

    Initial commit

---
+++ AnvilCC/Http/HttpServer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ComputerCraftRemote
+{
+    public abstract class HttpServer
+    {
+        protected int port;
+        TcpListener listener;
+        bool is_active = true;
+
+        public HttpServer(int port)
+        {
+            this.port = port;
+        }
+
+        public void listen()
+        {
+            listener = new TcpListener(port);
+            listener.Start();
+            while (is_active)
+            {
+                TcpClient s = listener.AcceptTcpClient();
+                HttpProcessor processor = new HttpProcessor(s, this);
+                Task.Factory.StartNew(processor.process);
+            }
+        }
+
+        public abstract void HandleGetRequest(HttpProcessor p);
+        public abstract void HandlePostRequest(HttpProcessor p, StreamReader inputData);
+    }
+
+}
+
+
+
