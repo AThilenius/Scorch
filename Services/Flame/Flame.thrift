708c6452a7f69278ce816958a5801531962e8b5c
Alec Thilenius <alec@thilenius.com>
2014-10-02 18:38:46 -0600

    Switching project over to CMake

---
+++ Services/Flame/Flame.thrift
@@ -0,0 +1,64 @@
+/**
+ *  bool        Boolean, one byte
+ *  byte        Signed byte
+ *  i16         Signed 16-bit integer
+ *  i32         Signed 32-bit integer
+ *  i64         Signed 64-bit integer
+ *  double      64-bit floating point value
+ *  string      String
+ *  binary      Blob (byte array)
+ *  map<t1,t2>  Map from one type to another
+ *  list<t1>    Ordered list of one type
+ *  set<t1>     Set of unique elements of one type
+ *
+ */
+namespace cpp Flame
+namespace csharp Flame
+
+
+/******************************************************************************/
+/* Structs                                                                    */
+/******************************************************************************/
+struct Spark {
+  1:i32 sparkID
+}
+
+struct Location {
+	1:i32 x,
+	2:i32 y,
+	3:i32 z
+}
+
+
+/******************************************************************************/
+/* Enums                                                                      */
+/******************************************************************************/
+enum MovementTypes {
+  Forward = 1,
+  Backward = 2,
+  TurnLeft = 3,
+  TurnRight = 4,
+  Up = 5,
+  Down = 6
+}
+
+
+/******************************************************************************/
+/* Services                                                                   */
+/******************************************************************************/
+service Flame {
+
+	/**************************************************************************/
+	/* Spark                                                                  */
+	/**************************************************************************/
+   	//===   Add / Remove   =====================================================
+   	Spark 			CreateSpark 			(1:Location worldLocation),
+   	list<Spark> 	GetAllSparks 			( ),
+   	void			RemoveSpark				(1:Spark sparkToRemove),
+   	void			RemoveAllSparks 		( ),
+
+
+   	//===   Movement   =========================================================
+   	bool 			DispatchMovementCommand	(1:Spark spark, 
+   											 2:MovementTypes movementCommand)
+}
