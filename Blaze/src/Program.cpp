da5344531ae9ddb3fd4ea273c19a5a14795d9eda
Alec Thilenius <alec@thilenius.com>
2014-10-02 20:54:45 -0600

    Got FlameCC working, and a demo of Blaze connecting to FlameCC and spinning a turtle.

--- Blaze/src/Program.cpp
+++ Blaze/src/Program.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
 
+#include <thrift/protocol/TBinaryProtocol.h>
+#include <thrift/transport/TSocket.h>
+#include <thrift/transport/TTransportUtils.h>
+
+#include "FlameService.h"
+#include "../../Visual Studio 2010/VC/include/vector"
+
+using namespace Flame;
+using namespace apache::thrift;
+using namespace apache::thrift::protocol;
+using namespace apache::thrift::transport;
+
 int main(int argc, char* argv[]) 
 {
-	std::cout << "Hello World" << std::endl;
+	boost::shared_ptr<TTransport> socket(new TSocket("localhost", 9090));
+	boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
+	boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+	FlameServiceClient client(protocol);
+
+	try {
+		transport->open();
+
+		Spark spark;
+		spark.sparkID = 6;
+
+		client.DispatchMovementCommand(spark, MovementTypes::Forward);
+		client.DispatchMovementCommand(spark, MovementTypes::Backward);
+		client.DispatchMovementCommand(spark, MovementTypes::Up);
+		client.DispatchMovementCommand(spark, MovementTypes::Down);
+		client.DispatchMovementCommand(spark, MovementTypes::TurnLeft);
+		client.DispatchMovementCommand(spark, MovementTypes::TurnRight);
+
+		std::vector<Spark> allSparks;
+		client.GetAllSparks(allSparks);
+
+		for (int i = 0; i < allSparks.size(); i++) {
+			std::cout << "Spark: " << allSparks[i].sparkID << std::endl;
+		}
+
+	} catch (TException& tx) {
+		std::cout << "ERROR: " << tx.what() << std::endl;
+	}
+
+	std::cin.ignore();
+	return 0;
 }
---
+++ FlameCC/FlameCC/FlameServiceHandler.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Flame
+{
+    public class FlameServiceHandler : FlameService.Iface
+    {
+        private QueueHttpServer m_httpServer;
+        private Dictionary<MovementTypes, String> m_movementCommandsLookup
+            = new Dictionary<MovementTypes, String>
+        {
+            { MovementTypes.Forward,    "return turtle.forward()" },
+            { MovementTypes.Backward,   "return turtle.back()" },
+            { MovementTypes.Up,         "return turtle.up()" },
+            { MovementTypes.Down,       "return turtle.down()" },
+            { MovementTypes.TurnLeft,   "return turtle.turnLeft()" },
+            { MovementTypes.TurnRight,  "return turtle.turnRight()" }
+        };
+
+        public FlameServiceHandler(QueueHttpServer httpServer) : base()
+        {
+            m_httpServer = httpServer;
+        }
+
+        public List<Spark> GetAllSparks()
+        {
+            List<Spark> turtles = new List<Spark>();
+
+            lock (m_httpServer.ComputerBuffersById)
+            {
+                foreach (var kvp in m_httpServer.ComputerBuffersById)
+                    turtles.Add(new Spark { SparkID = kvp.Key });
+            }
+
+            return turtles;
+        }
+
+        public bool DispatchMovementCommand(Spark spark, MovementTypes movementCommand)
+        {
+            bool retValue = true;
+
+            try
+            {
+                String retString = m_httpServer
+                    .RunCommand(spark.SparkID, m_movementCommandsLookup[movementCommand]);
+                retValue = bool.Parse(retString);
+            }
+            catch (Exception ex) { }
+
+            return retValue;
+        }
+
+        #region Not Suported By FlameCC
+
+        public Spark CreateSpark(Location worldLocation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveSpark(Spark sparkToRemove)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAllSparks()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
