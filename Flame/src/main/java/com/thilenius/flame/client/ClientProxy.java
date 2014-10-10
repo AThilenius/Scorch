2cd0dfd8d1dd798503ddbdc9474766b21c433456
Alec Thilenius <alec@thilenius.com>
2014-10-10 17:33:46 -0600

    Got the first version of Flame Java working

---
+++ Flame/src/main/java/com/thilenius/flame/client/ClientProxy.java
@@ -0,0 +1,10 @@
+package com.thilenius.flame.client;
+
+import com.thilenius.flame.CommonProxy;
+
+public class ClientProxy extends CommonProxy {
+	@Override
+	public void registerRenderers() {
+		// This is for rendering entities and so forth later on
+	}
+}
