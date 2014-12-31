2a7f9fc9bed964cd11c70ecb367fa15ad0510056
Alec Thilenius <Alec@Thilenius.com>
2014-12-30 19:15:42 -0700

    Updating IP for new EC2 instance.

--- Forge/app/assets/assignments/1/XCode/include/Config.h
+++ Forge/app/assets/assignments/1/XCode/include/Config.h
@@ -18,7 +18,7 @@ public:
     }
     static inline std::string GetBlazeIP() {
         // 127.0.0.0
-        return std::string("54.67.9.61");
+        return std::string("54.67.38.67");
     }
     static inline int GetBlazePort() {
         return 5529;
