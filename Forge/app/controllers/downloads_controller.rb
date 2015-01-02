6aaf64fd03ad6829727afe6b04416b49c12e46cd
Alec Thilenius <Alec@Thilenius.com>
2015-01-01 21:07:48 -0700

    Finished assignment compilation for all platforms. Testing still needed.

--- Forge/app/controllers/downloads_controller.rb
+++ Forge/app/controllers/downloads_controller.rb
@@ -3,27 +3,49 @@ class DownloadsController < ApplicationController
   require 'rubygems'
   require 'zip'
 
-  def compress(path, archive, replaceFile, authToken)
+  def compress(path, archive, replaceTokens)
     FileUtils.mkpath(File.dirname(archive))
     FileUtils.rm archive, :force=>true
 
     Zip::File.open(archive, Zip::File::CREATE) do |zipfile|
       Dir["#{path}/**/**"].reject{|f|f==archive}.each do |file|
-        if file =~ replaceFile
-          contents = File.read(file)
-          zipfile.get_output_stream(file.sub(path+'/','')) { |f| f.puts contents.gsub '<AuthToken>', authToken }
-        else
+
+        # If it's a directory just write it out
+        if File.directory? file
           zipfile.add(file.sub(path+'/',''), file)
+
+        else
+          # Read the file
+          contents = nil
+          File.open(file, 'rb') { |f| contents = f.read }
+
+          # Regex compare each replace request
+          replaceSelection = nil
+          replaceTokens.each do |fName, replaceArray|
+            if file =~ fName
+              replaceSelection = fName
+              break
+            end
+          end
+
+          # Replace contents with each request
+          if replaceSelection != nil
+            p replaceTokens[replaceSelection]
+            replaceTokens[replaceSelection].each do |from, to|
+              contents = contents.gsub(from, to)
+            end
+          end
+
+          # Write it out to the zip
+          zipfile.get_output_stream(file.sub(path+'/','')) { |f| f.puts contents }
         end
+
       end
     end
 
   end
 
-  def xcode
-    # Note: Path is formed as: assets/assignments/:id/:platform
-    return if sessionActiveCheckFailed
-
+  def sendProject(projectName)
     paramId = params[:id]
 
     # find assignment
@@ -34,24 +56,34 @@ class DownloadsController < ApplicationController
       return
     end
 
-    resourcePath = File.join(Rails.root, 'app', 'assets', 'assignments', "#{params[:id]}",  'XCode')
+    resourcePath = File.join(Rails.root, 'app', 'assets', 'assignments', "#{params[:id]}",  projectName)
     attachmentName = "#{sessionGetUser.lastName} Assignment #{params[:id]}.zip"
-    archive = File.join(Rails.root, 'tmp', 'assignments', "#{params[:id]}", 'XCode.zip')
+    archive = File.join(Rails.root, 'tmp', 'assignments', "#{params[:id]}", "#{projectName}.zip")
 
-    compress(resourcePath, archive, /Config.h/, userAssignment.authToken)
+    compress(resourcePath, archive, {/libAnvilLib.a/ =>
+                                         { 'D6089AA2-CB1C-4FFA-8990-4297A38376C2' => userAssignment.authToken }})
 
     # Send-er out!
     send_file(archive, :type => 'application/zip', :filename => attachmentName)
   end
 
+  def xcode
+    # Note: Path is formed as: assets/assignments/:id/:platform
+    return if sessionActiveCheckFailed
+
+    sendProject 'XCode'
+  end
+
   def vs
     return if sessionActiveCheckFailed
 
+    sendProject 'VS'
   end
 
   def makefile
     return if sessionActiveCheckFailed
 
+    sendProject 'Unix'
   end
 
   def flame
@@ -61,7 +93,9 @@ class DownloadsController < ApplicationController
     attachmentName = "#{sessionGetUser.lastName} Flame.zip"
     archive = File.join(Rails.root, 'tmp', 'Flame.zip')
 
-    compress(resourcePath, archive, /FlameConfig.json/, sessionGetUser.guid)
+    compress(resourcePath, archive, {/FlameConfig.json/ =>
+                             { '<AuthToken>' => sessionGetUser.guid }
+                         })
 
     # Send-er out!
     send_file(archive, :type => 'application/zip', :filename => attachmentName)
