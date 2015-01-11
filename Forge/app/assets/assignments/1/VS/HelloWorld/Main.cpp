ef82075b6f36c21c357399c97d89a19c157ac3af
Alec Thilenius <alec@thilenius.com>
2015-01-11 13:59:47 -0700

    Added Windows support for new AuthToken loading.

--- Forge/app/assets/assignments/1/VS/HelloWorld/Main.cpp
+++ Forge/app/assets/assignments/1/VS/HelloWorld/Main.cpp
@@ -3,12 +3,12 @@
 //  HelloWorld
 //
 //  Created by Alec Thilenius on 12/1/14.
-//  Copyright (c) 2014 Alec Thilenius. All rights reserved.
+//  Copyright (c) 2014 Scorch. All rights reserved.
 //
 #include "Anvil.h"
 
 using namespace AnvilAPI;
 
 int main() {
-	Anvil::LoadLevel(0);
+
 }
