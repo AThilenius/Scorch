3e424e65a83eedcf6a36373a3b3924d0d7a925e4
Alec Thilenius <alec@thilenius.com>
2015-05-13 22:16:39 -0700

    Added create_user code.

--- Auth/routes/authenticate.js
+++ Auth/routes/authenticate.js
@@ -1,21 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var utils = require('utils');
 
 // Post: /authenicate/create_ptoken
 router.post('/create_ptoken', function(req, res) {
-  console.log("create_ptoken");
+  if (!utils.fieldCheck(req, res, "email_address", "password")){
+    return;
+  }
+
   res.json( { hello : "create_ptoken" } );
 });
 
 // Post: /authenicate/create_stoken
 router.post('/create_stoken', function(req, res) {
-  console.log("create_stoken");
+  if (!utils.fieldCheck(req, res, "token")){
+    return;
+  }
   res.json( { hello : "create_stoken" });
 });
 
 // Post: /authenicate/validate_token
 router.post('/validate_token', function(req, res) {
-  console.log("validate_token");
+  if (!utils.fieldCheck(req, res, "token")){
+    return;
+  }
   res.json( { hello : "validate_tokens" });
 });
 
--- Auth/routes/user.js
+++ Auth/routes/user.js
@@ -1,28 +1,81 @@
 var express = require('express');
 var router = express.Router();
+var utils = require('utils');
 
 // Post: user/create
 router.post('/create', function(req, res) {
-  //var db = req.db;
-  //db.collection('user').find().toArray(function (err, items) {
-    //console.log(items);
-    //res.json(items)
-  //});
-  res.json( { hello : "create" } );
+  if (!utils.fieldCheck(req, res, "email_address", "password", "first_name",
+        "last_name")) {
+    return;
+  }
+
+  // Pre-process some fields
+  var emailAddress = req.body.email_address.replace(/ /g, '').toLowerCase();
+  var password = req.body.password;
+  var firstName = req.body.first_name.replace(/ /g, '');
+  var lastName = req.body.last_name.replace(/ /g, '');
+
+  if (password.length < 8) {
+    res.json(utils.error("Password must be at least 8 characters in length.",
+          400, null));
+    return;
+  }
+
+  var db = req.db.collection('users');
+
+  db.find({ email_address : emailAddress })
+    .toArray(function (err, items) {
+      if (utils.errorCheck(res, err)) {
+        return;
+      }
+
+      if (items.length !== 0) {
+        // User already exists.
+        res.json(utils.error("A user with the email address already exists." +
+              req.body.email_address + " already exists.", 403, null));
+        return;
+      } else { 
+        // User doesn't already exist, create it
+        var user = {
+          email_address : emailAddress,
+          password : password,
+          first_name : firstName,
+          last_name : lastName 
+        };
+        db.insert(user, function(err, records){
+          if (utils.errorCheck(res, err)) {
+            return;
+          } else {
+            res.json({ did_pass : true });
+            console.log("New account created: " + JSON.stringify(user, null, 2));
+            return;
+          }
+        });
+      }
+    });
 });
 
 // Post: user/read
 router.post('/read', function(req, res) {
+  if (!utils.fieldCheck(req, res, "token", "target_email_address")) {
+    return;
+  }
   res.json( { hello : "read" } );
 });
 
 // Post: user/update
 router.post('/update', function(req, res) {
+  if (!utils.fieldCheck(req, res, "token", "target_email_address")) {
+    return;
+  }
   res.json( { hello : "update" } );
 });
 
 // Post: user/delete
 router.post('/delete', function(req, res) {
+  if (!utils.fieldCheck(req, res, "token", "target_email_address")) {
+    return;
+  }
   res.json( { hello : "delete" } );
 });
 
