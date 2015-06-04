f02500167309262737934b79f0337b5fba4e2eba
Alec Thilenius <alec@thilenius.com>
2015-06-03 21:57:20 -0700

    Minor changes to servicer/auth building.

--- cloud/shared/js_libs/db-mongo.js
+++ cloud/shared/js_libs/db-mongo.js
@@ -1,17 +1,14 @@
 /*
  * Mongodb access through etcd
  */
-var flags = require('flags');
 var mongo = require('mongoskin');
 
-flags.defineString('mongo_ip', 'localhost',
-  'The ip address or hostname of the MongoDB instance');
-flags.defineInteger('mongo_port', 27017, 'The port of the MongoDB instance');
-flags.parse();
+console.log("NodeJS MongoDB using ip: [" + process.env.MONGO_IP + "], port: [" +
+  process.env.MONGO_PORT + "]");
 
 exports.getDbByName = function(dbName) {
-  return mongo.db("mongodb://" + process.env.MONGO_IP + ":" + process.env.MONGO_PORT +
-    "/" + dbName, {
+  return mongo.db("mongodb://" + process.env.MONGO_IP + ":" +
+    process.env.MONGO_PORT + "/" + dbName, {
       native_parser: true
     });
 };
